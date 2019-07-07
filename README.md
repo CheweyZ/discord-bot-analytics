1e1c0301c20ded74004e721428ec027514e9661f
Unknown <cheweyzak002@gmail.com>
2019-07-07 14:53:40 +1200

    Here we go

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # discord-bot-analytics
 Record discord bot analytics with ease
+
+
+# Getting started
+To first get started you will need a key to use https://api.chewey-bot.ga/ (Free)
+
+
+If you are using Discord.js or Eris then this module is already built with full auto reporting
+
+To get started 
+```
+const cheweyBotAnalyticsAPI=require("discord-bot-analytics")
+const customAnalytics = new cheweyBotCustomAPILib("YOUR API TOKEN", discordBot)
+```
+If you using Discord.js or Eris then thats all you rreporting is setup
+
+# How to access my analytics
+Webdash board (soon)
+
+Get all stats for chart (fetch every 5 minutes)
+https://api.chewey-bot.ga/analytics/getall/YOUR-USERID
+
+Getting latest (fetch every minute)
+https://api.chewey-bot.ga/analytics/getlatest/YOUR-USERID
+
+# What can I track
+Currently supported: servers, users, channels, sent messages, received messages, Ram
+
+#Details
+Analytics are reported at 10minute intervals and stored forever (Details below)
+
+As time goes on the stats will become more aggregated when stored on the server (Up per day stats)
+    This is so more data can be held longer giving better insight to your bot
+    As you may not care about per hour data from 6 months ago hence as time goes it might be compressed to for that day
+
+# Whats the catch
+There is none as long as I can pay the bills then this service will be free 
+Want like the service or want to help out then checkout https://www.patreon.com/CheweyZ
+
+Perks coming soon: Higher resolution data (per minute)
---
+++ index.js
@@ -0,0 +1,135 @@
+const https = require('https')
+
+/**
+ * @typedef AnalyticData
+ * @property {Number} servers
+ * @property {Number} users
+ * @property {Number} channels
+ * @property {Number} sent_messages
+ * @property {Number} received_messages
+ * @property {Number} ram_used
+ */
+
+class Analytics{
+    /**
+     * @param {String} apiToken Chewey Bot api Token
+     * @param {import("eris").Client | import("discord.js").Client} discordBot (recommended) optional if want auto reporter (stats recorded for you)
+     */
+    constructor(apiToken,discordBot){
+        this.options={
+            apiToken,
+            discordBot:discordBot,
+            sent_messages:0,
+            received_messages:0
+        }
+        if(discordBot!=null){
+            discordBot.on("messageCreate",(msg)=>{
+                this.options.received_messages++;
+                if(msg.author.id==discordBot.user.id){
+                    this.options.sent_messages++;
+                }
+            })
+            // allow bot to warmup
+            setTimeout(() => {
+                this.startAutoReport()
+                // send initial on load
+                this.sendReport(this.buildBody(this.options.discordBot)).then(() => {}, () => {})
+            }, 5000);
+        }
+    }
+
+    /**
+     * Start auto reporting - only used if discordBot Client provided
+     */
+    startAutoReport(){
+        if (this.options.discordBot && !this.options.interval) {
+            this.options.interval = setInterval(() => {
+                this.sendReport(this.buildBody(this.options.discordBot)).then(() => {}, () => {}) //ignore auto errors
+            }, 10.1 * 60 * 1000); //10minute + extra to ensure clean limits
+        }
+    }
+
+    /**
+     * Stop auto reporting - to start again use startAutoReport()
+     */
+    stopAutoReport(){
+        if (this.options.interval){
+            clearInterval(this.options.interval)   
+            delete this.options.interval
+        }
+    }
+
+    /**
+     * Used to auto construct the body to send
+     * @param {@param {import("eris").Client | import("discord.js").Client} discordBot} discordBot
+     * @returns {AnalyticData}
+     */
+    buildBody(discordBot){
+        // Eris : Discord.js
+        let channelCount = discordBot.channelGuildMap ? Object.keys(discordBot.channelGuildMap).length : discordBot.channels.size
+        let reply= {
+            servers:discordBot.guilds.size,
+            users:discordBot.users.size,
+            channels:channelCount,
+            sent_messages:this.options.sent_messages,
+            received_messages: this.options.received_messages,
+            ram_used:process.memoryUsage().rss //not true usage but is true ram consumption by pc spec
+        }
+        this.options.sent_messages=0;
+        this.options.received_messages=0;
+        return reply
+    }
+    /**
+     * @param {AnalyticData} body
+     * @returns {Promise<String|null>}
+     */
+    sendReport(body){
+        console.log("Sending rep",body);
+        
+        return httpsPost(body,this.options.apiToken)
+    }
+}
+function httpsPost(body,auth){
+    return new Promise((resolve,reject)=>{
+        const data = JSON.stringify(body)
+        const options = {
+            hostname: 'api.chewey-bot.ga', //"api.localhost.com"
+            port: 443, //8080,
+            path: '/analytics/post',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': data.length,
+                'Authorization': auth
+            }
+        }
+        // const http=require("http")
+        const req = https.request(options, (res) => {
+            let body = '';
+            res.on('data', function (d) {
+                body += d;
+            });
+            res.on("end",()=>{
+                try{
+                    body=JSON.parse(body)
+                }catch(e){}
+                if(res.statusCode!=200){
+                    console.log("Report stats Chewey Bot API error ",body);
+                }
+                resolve(body)
+            })
+        })
+        
+        req.on('error', (error) => {
+            console.error("Report stats Chewey Bot API Error",error)
+            reject(error)
+        })
+        
+        req.write(data)
+        req.end()
+
+    })
+}
+
+
+module.exports=Analytics
